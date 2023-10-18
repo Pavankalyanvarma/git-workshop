5b09f019c61ff38f9210af06ef4baa6cd6502970
MudUnuru Pavan Kalyan Varma <pavanvarma@leewayhertz.com>
2023-10-18 17:12:44 +0530

    Resolved conflicts with new-feature branch

--- Array.js
+++ Array.js
@@ -1,8 +1,10 @@
 function getArray() {
     return [1,2,3,4,5,6,7,8];
+    
   }
   function AlphaArray(){
     return [a,b,c];
+
   }
   const myArray = getArray();
   const myAlphaArray= AlphaArray();
